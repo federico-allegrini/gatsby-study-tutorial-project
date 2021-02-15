1a504011e9125802a39147bd8157b91628ed1832
Federico Allegrini <federicoallegrini@mondoconv.it>
2021-02-15 18:05:21 +0100

    Link

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,9 +1,11 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const blog = () => {
   return (
     <div>
       <h1>Blog page</h1>
+      <Link to="/">Home page</Link>
     </div>
   )
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
 
 export default function Home() {
-  return <div>Hello people!</div>
+  return (
+    <div>
+      Hello people!
+      <Link to="/blog/">Blog page</Link>
+      <a href="https://www.gatsbyjs.com/">Gatsby docs</a>
+    </div>
+  )
 }
