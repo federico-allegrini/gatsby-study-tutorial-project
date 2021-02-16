4cde835ab00e70ee7facf1fa4b82eb62627b43bb
Federico Allegrini <federicoallegrini@mondoconv.it>
2021-02-16 18:36:41 +0100

    Navbar

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import React from "react"
 import { Link } from "gatsby"
+import Navbar from "../components/Navbar"
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       Hello people!
       <Link to="/blog/">Blog page</Link>
       <a href="https://www.gatsbyjs.com/">Gatsby docs</a>
